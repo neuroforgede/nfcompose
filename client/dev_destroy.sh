f94e2a6a8b33166e98a54e9c438b798c7c2145da
Martin Braun <braun@neuroforge.de>
2023-05-15 23:17:08 +0200

    fix paths in client unit tests

--- client/dev_destroy.sh
+++ client/dev_destroy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd ../deploy
+cd ../deploy/local
 DEV_NETWORK_NAME=$(whoami)_client_dev_nfcompose COMPOSE_PROJECT_NAME=$(whoami)_client_dev CONTAINER_USER_ID=$(id -u) CONTAINER_GROUP_ID=$(id -g) bash destroy.sh
